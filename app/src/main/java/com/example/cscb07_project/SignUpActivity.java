9847fb999b924803ddc32b43b950bcb89ad5288c
usr77868 <claude.zhu@mail.utoronto.ca>
2024-11-14 16:13:39 -0500

    feat: Implement login validation in LogInActivity And Fixed Redirect Text In SignUpView
    - Set up UI components for email and password input
    - Initialize LoginPresenter in LogInActivity
    - Add click listener for login button to trigger validateCredentials
    - Handle UI updates and navigation in response to login attempts
    - Fix the behaviour of RedirectLogIn

--- app/src/main/java/com/example/cscb07_project/SignUpActivity.java
+++ app/src/main/java/com/example/cscb07_project/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.cscb07_project;
 //import android.util.Patterns;
 import android.content.Intent;
 import java.util.regex.Pattern;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,13 +11,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
@@ -41,13 +42,15 @@ public class SignUpActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         mAuth = FirebaseAuth.getInstance();
-        fullName = findViewById(R.id.fullName);
+
+        fullName = findViewById(R.id.CredentialEmail);
         signUpEmail = findViewById(R.id.signUpEmail);
-        signUpPassword = findViewById(R.id.signUpPassword);
+        signUpPassword = findViewById(R.id.logInPassword);
         confirmPassword = findViewById(R.id.confirmPassword);
-        signUpButton = findViewById(R.id.signUpButton);
-        redictLogIn = findViewById(R.id.redictLogIn);
+        signUpButton = findViewById(R.id.logInButton);
+        redictLogIn = findViewById(R.id.redirectToSignUp);
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
