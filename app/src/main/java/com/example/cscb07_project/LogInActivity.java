6f8e0ce95ddf71f6747b2f08fb82079256ca80c9
usr77868 <claude.zhu@mail.utoronto.ca>
2024-11-16 15:18:14 -0500

    add forget password feature

--- app/src/main/java/com/example/cscb07_project/LogInActivity.java
+++ app/src/main/java/com/example/cscb07_project/LogInActivity.java
@@ -1,24 +1,13 @@
 package com.example.cscb07_project;
 
 import android.content.Intent;
-import java.util.regex.Pattern;
-import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity implements LogInView {
 
@@ -33,9 +22,9 @@ public class LogInActivity extends AppCompatActivity implements LogInView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        emailEditText = findViewById(R.id.CredentialEmail);
+        emailEditText = findViewById(R.id.ResetPasswordCredentialEmail);
         passwordEditText = findViewById(R.id.logInPassword);
-        loginButton = findViewById(R.id.logInButton);
+        loginButton = findViewById(R.id.ResetPasswordButton);
         redirectToSignUp = findViewById(R.id.redirectToSignUp);
 
         presenter = new LogInPresenter(this);
